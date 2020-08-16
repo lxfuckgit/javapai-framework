aaaf5880ef3cba535d06e9bd7af153964414bb42
yonghuxiang <xiang19999>
2020-08-16 12:02:45 +0800

    开放public修饰.

--- javapai-framework-common/src/main/java/com/javapai/framework/constant/CL_Formater.java
+++ javapai-framework-common/src/main/java/com/javapai/framework/constant/CL_Formater.java
@@ -4,15 +4,15 @@ public final class CL_Formater {
 	/**
 	 * 默认格式:yyyy-MM-dd<br>
 	 */
-	final static String FORMAT_1 = "yyyy-MM-dd";
+	public final static String FORMAT_1 = "yyyy-MM-dd";
 	
 	/**
 	 * 默认格式:yyyy-MM-dd HH:mm:ss<br>
 	 */
-	final static String FORMAT_2 = "yyyy-MM-dd HH:mm:ss";
+	public final static String FORMAT_2 = "yyyy-MM-dd HH:mm:ss";
 	
 	/**
 	 * 默认格式:yyyy-MM-dd kk:mm:ss<br>
 	 */
-	final static String FORMAT_3 = "yyyy-MM-dd kk:mm:ss";// 此格式为跨平台的时间格式，用kk而不用hh是因为kk是24进制的而不随操作系统设置变动。
+	public final static String FORMAT_3 = "yyyy-MM-dd kk:mm:ss";// 此格式为跨平台的时间格式，用kk而不用hh是因为kk是24进制的而不随操作系统设置变动。
 }
