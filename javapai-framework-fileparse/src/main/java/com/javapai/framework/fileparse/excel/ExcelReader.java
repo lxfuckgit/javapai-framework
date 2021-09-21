92f4f1ef9c23c81f3c04d4c851262dbf9f7d1503
lxfuckgit <pooja@windows7>
2021-09-21 17:15:40 +0800

    1:增加默认接口方法：getDefaultSheetIndex 2:对相关接口增加接口使用说明。

--- javapai-framework-fileparse/src/main/java/com/javapai/framework/fileparse/excel/ExcelReader.java
+++ javapai-framework-fileparse/src/main/java/com/javapai/framework/fileparse/excel/ExcelReader.java
@@ -28,19 +28,53 @@ import com.javapai.framework.fileparse.office.OfficeReader;
  * 
  */
 public interface ExcelReader extends OfficeReader<TableFormat> {
+	/**
+	 * 
+	 * @param file
+	 * @param sheetIndex
+	 * @return
+	 */
 	public TableFormat readSheet(File file, int sheetIndex);
 	
+	/**
+	 * 
+	 * @param file
+	 * @param sheetIndex
+	 * @param config
+	 * @return
+	 */
 	public TableFormat readSheet(File file, int sheetIndex, SheetConfig config);
 	
-	public TableFormat readSheet(InputStream strean, int sheetIndex);
+	/**
+	 * 指取指定input输入流中的指定sheent表单。<br?
+	 * 
+	 * @param inputStream
+	 *            输入流。<br>
+	 * @param sheetIndex
+	 *            表单序号（默认第1个表单序号为1）。<br>
+	 * @return 格式化后的表单内容。
+	 */
+	public TableFormat readSheet(InputStream inputStream, int sheetIndex);
 
-	public TableFormat readSheet(InputStream strean, int sheetIndex, SheetConfig config);
+	public TableFormat readSheet(InputStream inputStream, int sheetIndex, SheetConfig config);
 
-	public TableFormat readSheet(InputStream strean, String sheetName);
+	public TableFormat readSheet(InputStream inputStream, String sheetName);
 
-	public TableFormat readSheet(InputStream strean, String sheetName, SheetConfig config);
+	public TableFormat readSheet(InputStream inputStream, String sheetName, SheetConfig config);
 	
 	//未开放原因:通过类实例来绑定config，再调用：readFile(File file)即可。
 //	public java.util.List<TableFormat> readFile(File file, SheetConfig config);
+	
+	/**
+	 * 返回默认sheet索引标识号。<br>
+	 * 
+	 * @param sheetIndex
+	 *            用户指定sheet索引标识号。<br>
+	 * <strong>注意：</strong>如果sheetIndex参数未输入或输入小于1，则默认sheetIndex=1；原因是友好的面向使用者。<br>
+	 * @return sheet索引标识号。<br>
+	 */
+	default int getDefaultSheetIndex(int sheetIndex) {
+		return sheetIndex <= 0 ? 1 : sheetIndex;
+	};
 
 }
