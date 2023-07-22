c2090239cd4fddd840189122478e6e3d0c7fea54
pooja <pooja@localhost>
2023-07-22 16:07:30 +0800

    坐标类增加。

---
+++ javapai-framework-common/src/main/java/com/javapai/framework/common/vo/coordinate/CoordinateVO.java
@@ -0,0 +1,29 @@
+package com.javapai.framework.common.vo.coordinate;
+
+public class CoordinateVO {
+	/**
+	 * X坐标。
+	 */
+	private long x;
+	/**
+	 * Y坐标。
+	 */
+	private long y;
+
+	public long getX() {
+		return x;
+	}
+
+	public void setX(long x) {
+		this.x = x;
+	}
+
+	public long getY() {
+		return y;
+	}
+
+	public void setY(long y) {
+		this.y = y;
+	}
+
+}
