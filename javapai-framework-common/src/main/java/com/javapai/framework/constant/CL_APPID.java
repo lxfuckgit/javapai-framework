1d4c81c9c9981d5c32f86030eefea19a2d6c881c
pooja <pooja@localhost>
2023-08-11 19:00:27 +0800

    枚举类AppIdEnum由CLAPPID代替。

---
+++ javapai-framework-common/src/main/java/com/javapai/framework/constant/CL_APPID.java
@@ -0,0 +1,8 @@
+package com.javapai.framework.constant;
+
+public final class CL_APPID {
+	/**
+	 * 默认APP应用标识。<br>
+	 */
+	public static final String DEFUALT_APP = "defalut_app";
+}
---
+++ javapai-framework-common/src/main/java/com/javapai/framework/constant/CL_Common.java
@@ -0,0 +1,11 @@
+package com.javapai.framework.constant;
+
+/**
+ * 通用常量类。<br>
+ * 建议：常量分类存放，实在没地儿放且又通用就先放这。<br>
+ * 
+ * @author pooja
+ *
+ */
+public class CL_Common {
+}
