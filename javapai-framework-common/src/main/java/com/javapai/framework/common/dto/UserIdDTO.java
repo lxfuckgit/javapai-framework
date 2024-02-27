1ed1d5b756e93893c0404c82a554ab5a64a57b01
pooja <pooja@localhost>
2024-02-27 09:48:38 +0800

    long update to Long!

--- javapai-framework-common/src/main/java/com/javapai/framework/common/dto/UserIdDTO.java
+++ javapai-framework-common/src/main/java/com/javapai/framework/common/dto/UserIdDTO.java
@@ -18,11 +18,11 @@ public final class UserIdDTO extends BaseRstDTO implements Serializable {
 	@NotNull
 	private Long userId;
 
-	public long getUserId() {
+	public Long getUserId() {
 		return userId;
 	}
 
-	public void setUserId(long userId) {
+	public void setUserId(Long userId) {
 		this.userId = userId;
 	}
 }
